269e5f1a2822cb5dd670664a3a2520ce27d2905e
pghataorre <pghataorre@gmail.com>
2025-07-02 18:30:41 +0100

    changed the apiUrl - fix attempt

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: '/api',
+    apiUrl: process.env.ENVIRONMENT === 'prod' ? '/api' : 'http://localhost:3000/api',
     baseServerApi: process.env.BASE_JSON_SERVER_URL || 'http://localhost:3001',
     permyApi: process.env.PERMY_API || 'https://api.permy.co.uk/mixCount',
     productApi: process.env.PRODUCT_URL || 'https://dummyjson.com/products',
