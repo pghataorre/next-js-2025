baeb29973e58211a1207ec5b8f22248b93a7551c
pghataorre <pghataorre@gmail.com>
2025-07-30 16:59:26 +0100

    sylinfg fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <PageLayout>
       <h1>NEXT JS TRAINING MAY 2025</h1>
-      <p>sadisids</p>
+      <p>Next JS Training app with various mini page applciations</p>
     </PageLayout>
   );
 }
